49be183a8403d5949e6bb6f47a1505827165be83
tea9 <shaomiaojava@gmail.com>
2018-08-30 12:42:02 +0800

    drafts  shell

--- rakefile.rb
+++ rakefile.rb
@@ -4,6 +4,7 @@ require 'yaml'
 SOURCE = "."
 CONFIG = {
   'posts' => File.join(SOURCE, "_posts"),
+  'drafts' => File.join(SOURCE, "_drafts"),
   'post_ext' => "md",
 }
 
@@ -35,3 +36,30 @@ task :post do
 
   
 end # task :post
+
+## 第二个命令
+desc "Begin a new post in #{CONFIG['drafts']}"
+task :draft do
+  abort("rake aborted: '#{CONFIG['drafts']}' directory not found.") unless FileTest.directory?(CONFIG['drafts'])
+  title = ENV["title"] || "new-post"
+  slug = title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  filename = File.join(CONFIG['drafts'], "#{Time.now.strftime('%Y-%m-%d')}-#{slug}.#{CONFIG['post_ext']}")
+  if File.exist?(filename)
+    abort("rake aborted!") if ask("#{filename} already exists. Do you want to overwrite?", ['y', 'n']) == 'n'
+  end
+
+  puts "Creating new post: #{filename}"
+
+
+  open(filename, 'w') do |post|
+    post.puts "---"
+    post.puts "layout: post"
+    post.puts "title: \"#{title.gsub(/-/,' ')}\""
+    # post.puts "subtitle: \"\""
+    post.puts "date: #{Time.now.strftime('%Y-%m-%d')}"
+    # post.puts "cover: "
+    post.puts "category: "
+    post.puts "tags: "
+    post.puts "---"
+  end
+end

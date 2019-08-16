56e0674f02d02e606204b82d77cc33edf137f1fb
tea9 <shaomiaojava@gmail.com>
2019-08-16 14:24:25 +0800

    Site updated: 2019-08-16 14:24:19

--- js/index.js
+++ js/index.js
@@ -238,7 +238,7 @@ var tocToTop = getDistanceOfLeft(toc).top;
 
 function reHeightToc(){
     if(toc) { // resize toc height
-        toc.style.height = ( document.documentElement.clientHeight - 10 ) + 'px';
+        toc.style.maxHeight = ( document.documentElement.clientHeight - 10 ) + 'px';
         toc.style.overflowY = 'scroll';
     }
 }
@@ -340,8 +340,10 @@ if(window.isPost){
 
         reLayout()
 
-        window.addEventListener('scroll', (e) => {
+        window.addEventListener('scroll', function(e) {
             reLayout()
+            // let tocDom = document.querySelector('#toc')
+            // window.scrollY < 550 ? tocDom.classList.remove('toc-fixed') : tocDom.classList.add('toc-fixed')
         })
     }
 }
